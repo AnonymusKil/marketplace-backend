64f9a32562ce8f8ac060fe531ae45188e9316a90
Anonymuskil <navadesignz11@gmail.com>
2026-06-21 08:32:29 -0700

    changed the gemini AI model to 3.5

--- src/services/geminiService.ts
+++ src/services/geminiService.ts
@@ -1,13 +1,18 @@
 import { model } from "../config/gemini.js";
 
-export async function generateProductDescriptionWithAi(productName: string){
+export async function generateProductDescriptionWithAi(productName: string) {
   const prompt = `
-    Write a short, high-converting e-commerce product description for:
-    Product: ${productName}
-    Keep it clear, modern, and persuasive.
-  `;
+Write a high-converting e-commerce product description in 2 sentences.
 
-  const content = await model.generateContent(prompt)
-  const result = await content.response
+Product: ${productName}
+
+Rules:
+- 2 sentences only
+- Make it persuasive and modern
+- Focus on benefits, not features
+- No fluff, no storytelling
+`;
+  const content = await model.generateContent(prompt);
+  const result = await content.response;
   return result.text();
 }
