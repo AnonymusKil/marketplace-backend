d7bd7ad26592a186d5952a55ef1b194c300028f1
Anonymuskil <navadesignz11@gmail.com>
2026-06-28 13:51:58 -0700

    added new thing

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,12 +1,20 @@
 import productModel from "../model/productModel.js";
 import Usermodel from "../model/Usermodel.js";
-import sellermodel from "../model/sellerModel.js"
-import {generateProductDescriptionWithAi} from "./geminiService.js"
+import sellermodel from "../model/sellerModel.js";
+import { generateProductDescriptionWithAi } from "./geminiService.js";
 interface CreateProductInput {
   name: string;
   description: string;
   price: number;
-  category: "Electronics" | "Clothing" | "Home & Kitchen" | "Beauty & Health" | "Toys & Games" | "Sports & Outdoors" | "Books & Media" | "Food & Drink";
+  category:
+    | "Electronics"
+    | "Clothing"
+    | "Home & Kitchen"
+    | "Beauty & Health"
+    | "Toys & Games"
+    | "Sports & Outdoors"
+    | "Books & Media"
+    | "Food & Drink";
   images: string[];
   publicId: string;
 }
@@ -42,17 +50,25 @@ export async function createProduct(
     const seller = await sellermodel.findOne({ owner: userId });
     if (!seller) throw new Error("Seller not found");
     // check if product with same name exists for this seller
-    const existingProduct = await productModel.findOne({ name: name.trim(), seller: seller._id });
+    const existingProduct = await productModel.findOne({
+      name: name.trim(),
+      seller: seller._id,
+    });
     if (existingProduct) {
       throw new Error("You already have a product with this name");
     }
-    const productdescription = await generateProductDescriptionWithAi(name)
+    let aiDescription = "";
 
+    try {
+      aiDescription = await generateProductDescriptionWithAi(name);
+    } catch (err) {
+      console.log("Gemini generation failed:", err);
+    }
 
     // Create the product
     const product = await productModel.create({
       name: name.trim(),
-      description: productdescription,
+      description: aiDescription || description,
       price,
       category,
       images,
